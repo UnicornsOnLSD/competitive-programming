344e4fff0471f003a285a7e84d8efe41a025d135
James Harvey <44349936+UnicornsOnLSD@users.noreply.github.com>
2026-09-20 00:51:38 +0100

    AoC 2019 Day 2 Part 2

--- advent-of-code/2019/day2/main.cpp
+++ advent-of-code/2019/day2/main.cpp
@@ -9,86 +9,160 @@
 #include <vector>
 
 class IntcodeCpu {
-public:
-  uint64_t pc = 0;
-  bool has_halted = false;
-  std::vector<uint64_t> memory;
+  public:
+    IntcodeCpu(const std::string &program_str)
+        : IntcodeCpu(parse_program_string(program_str)) {}
 
-  IntcodeCpu(const std::string &program) {
-    for (const auto opcode_str : std::views::split(program, ',')) {
-      uint64_t opcode;
+    IntcodeCpu(std::vector<uint64_t> program) : memory(std::move(program)) {}
 
-      auto [ptr, ec] = std::from_chars(
-          opcode_str.data(), opcode_str.data() + opcode_str.size(), opcode);
+    // Getters and setters 😍
 
-      if (ec != std::errc{}) {
-        throw std::system_error(std::make_error_code(ec));
-      }
+    /// Returns the "output" of the program, which AoC seems to use as position
+    /// 0.
+    uint64_t get_output() { return memory[0]; }
 
-      memory.push_back(opcode);
+    uint64_t get_noun() { return memory[1]; }
+    void set_noun(uint64_t val) { memory[1] = val; }
+
+    uint64_t get_verb() { return memory[2]; }
+    void set_verb(uint64_t val) { memory[2] = val; }
+
+    // Debug functions
+
+    /// Prints out the program's memory to standard output.
+    void dump_memory() {
+        bool is_first = true;
+
+        for (const auto &opcode : memory) {
+            if (!is_first) {
+                std::cout << ',';
+            }
+
+            is_first = false;
+
+            std::cout << opcode;
+        }
+
+        std::cout << '\n';
     }
-  }
 
-  void dump_memory() {
-    for (const auto &opcode : memory) {
-      std::cout << opcode << ',';
+    // Control functions
+
+    /// Runs the program until it reaches a halt instruction.
+    void run_until_halt() {
+        while (!has_halted) {
+            step();
+        }
     }
 
-    std::cout << '\n';
-  }
+    /// Parses a program string (from AoC input) into a vector of opcodes.
+    static std::vector<uint64_t>
+    parse_program_string(const std::string &program_str) {
+        std::vector<uint64_t> out;
 
-  void run_until_halt() {
-    while (!has_halted) {
-      step();
+        for (const auto opcode_str : std::views::split(program_str, ',')) {
+            uint64_t opcode;
+
+            auto [ptr, ec] =
+                std::from_chars(opcode_str.data(),
+                                opcode_str.data() + opcode_str.size(), opcode);
+
+            if (ec != std::errc{}) {
+                throw std::system_error(std::make_error_code(ec));
+            }
+
+            out.push_back(opcode);
+        }
+
+        return out;
     }
-  }
 
-  void step() {
-    switch (memory[pc]) {
-    case 1:
-      add();
-      break;
-    case 2:
-      mult();
-      break;
-    case 99:
-      halt();
-      break;
-    default:
-      throw std::logic_error(std::format("Invalid instruction {}", memory[pc]));
+  private:
+    /// Instruction pointer
+    uint64_t ip = 0;
+
+    /// Whether or not we've reached a halt instruction
+    bool has_halted = false;
+
+    /// The program's memory
+    std::vector<uint64_t> memory;
+
+    // Control functions
+
+    /// Run the next instruction in the program
+    void step() {
+        switch (memory[ip]) {
+        case 1:
+            add();
+            break;
+        case 2:
+            mult();
+            break;
+        case 99:
+            halt();
+            break;
+        default:
+            throw std::logic_error(
+                std::format("Invalid instruction {}", memory[ip]));
+        }
     }
-  }
 
-private:
-  void add() {
-    auto a = memory[memory[pc + 1]];
-    auto b = memory[memory[pc + 2]];
+    // Instructions (in order of opcode number)
 
-    memory[memory[pc + 3]] = a + b;
-    pc += 4;
-  }
+    /// Adds the value of the addresses in operand A and B into the address
+    /// specified by operand C.
+    void add() {
+        auto a = memory[memory[ip + 1]];
+        auto b = memory[memory[ip + 2]];
 
-  void mult() {
-    auto a = memory[memory[pc + 1]];
-    auto b = memory[memory[pc + 2]];
+        memory[memory[ip + 3]] = a + b;
+        ip += 4;
+    }
 
-    memory[memory[pc + 3]] = a * b;
-    pc += 4;
-  }
+    /// Multiplies the value of the addresses in operand A and B into the
+    /// address specified by operand C.
+    void mult() {
+        auto a = memory[memory[ip + 1]];
+        auto b = memory[memory[ip + 2]];
 
-  void halt() { has_halted = true; }
+        memory[memory[ip + 3]] = a * b;
+        ip += 4;
+    }
+
+    /// Halts the CPU
+    void halt() { has_halted = true; }
 };
 
 int main(int argc, char *argv[]) {
-  std::string opcodes_str;
-  std::getline(std::cin, opcodes_str);
+    std::string program_str;
+    std::getline(std::cin, program_str);
+    auto program = IntcodeCpu::parse_program_string(program_str);
 
-  auto cpu = IntcodeCpu(opcodes_str);
+    auto cpu = IntcodeCpu(program);
 
-  cpu.memory[1] = 12;
-  cpu.memory[2] = 2;
-  
-  cpu.run_until_halt();
+    cpu.set_noun(12);
+    cpu.set_verb(2);
 
-  std::cout << cpu.memory[0] << '\n';
+    cpu.run_until_halt();
+
+    std::cout << cpu.get_output() << '\n';
+
+    for (int noun = 0; noun < 100; noun++) {
+        for (int verb = 0; verb < 100; verb++) {
+            cpu = IntcodeCpu(program);
+
+            cpu.set_noun(noun);
+            cpu.set_verb(verb);
+
+            cpu.run_until_halt();
+
+            if (cpu.get_output() == 19690720) {
+                std::cout << 100 * noun + verb << '\n';
+                return 0;
+            }
+        }
+    }
+
+    std::cout << "Failed to find part 2 answer :(\n";
+    return 1;
 }
