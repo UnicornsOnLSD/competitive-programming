64a5b4d8b79d4eadaa209b87322608a62b01d77f
James Harvey <44349936+UnicornsOnLSD@users.noreply.github.com>
2026-09-19 21:59:37 +0100

    AoC 2019 Day 1 Part 2

--- advent-of-code/2019/day1/main.cpp
+++ advent-of-code/2019/day1/main.cpp
@@ -2,13 +2,25 @@
 #include <string>
 
 int main(int argc, char *argv[]) {
-  std::string mass_str;
-  int result = 0;
+    std::string mass_str;
+    int part1_result = 0;
+    int part2_result = 0;
 
-  while (std::getline(std::cin, mass_str)) {
-      int mass = std::stoi(mass_str);
-      result += mass / 3 - 2;
-  }
+    while (std::getline(std::cin, mass_str)) {
+        int mass = std::stoi(mass_str);
+        int fuel_cost = mass / 3 - 2;
 
-  std::cout << result << '\n';
+        part1_result += fuel_cost;
+        part2_result += fuel_cost;
+
+        while (fuel_cost > 0) {
+            fuel_cost = fuel_cost / 3 - 2;
+
+            if (fuel_cost > 0) {
+                part2_result += fuel_cost;
+            }
+        }
+    }
+
+    std::cout << part1_result << '\n' << part2_result << '\n';
 }
